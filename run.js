6e82075c4333cdb638ca32ac37cc0fc00c50c92e
Shelley Vohr <shelley.vohr@gmail.com>
2020-04-30 18:57:09 -0700

    fix: handle symlinks when comparing to __filename

--- run.js
+++ run.js
@@ -1,9 +1,15 @@
 #!/usr/bin/env node
 
+const { existsSync, realpathSync } = require('fs');
 const { homedir } = require('os');
-const path = require('path');
+const { resolve }= require('path');
 
-const ePath = path.resolve(homedir(), '.electron_build_tools', 'src', 'e');
-process.argv = process.argv.map(arg => arg === __filename ? ePath : arg);
+const ePath = resolve(homedir(), '.electron_build_tools', 'src', 'e');
+process.argv = process.argv.map(arg => {
+  if (existsSync(arg)) {
+    return realpathSync(arg) === realpathSync(__filename) ? ePath : arg;
+  }
+  return arg;
+});
 
 require(ePath);
