d964f9cb09f9aaee3be98868ed463956112fa8dd
Shelley Vohr <shelley.vohr@gmail.com>
2020-05-04 10:57:40 -0700

    Use Node.js assert instead of chai

--- test/module.test.js
+++ test/module.test.js
@@ -1,5 +1,4 @@
-const { expect } = require('chai');
-
+const assert = require('assert');
 const { homedir } = require('os');
 const { existsSync } = require('fs');
 const { execSync } = require('child_process');
@@ -7,17 +6,16 @@ const { resolve } = require('path');
 
 it('installs build-tools to the home directory', () => {
   const buildToolsDir = resolve(homedir(), '.electron_build_tools');
-  expect(existsSync(buildToolsDir)).to.be.true;
+  const dirExists = existsSync(buildToolsDir);
+  assert.strictEqual(dirExists, true);
 });
 
 it('adds e to the user PATH', () => {
   const path = execSync('which e');
-  expect(path).to.not.be.empty;
+  assert.strictEqual(path.length === 0, false);
 });
 
 it('can execute e', () => {
   const info = execSync('e');
-  const helpMenuTitle = 'Electron build tool';
-
-  expect(info.includes(helpMenuTitle)).to.be.true;
+  assert.match(info.toString(), /Electron build tool/);
 });
