e3aec2062d32f93c7ffc088f51a7ebb4305c4c43
Samuel Attard <sam@electronjs.org>
2025-07-01 17:35:55 -0700

    feat: add support for different clone locations (#47)
    * feat: add support for different clone locations
    
    * oops
    
    * ok...

--- run.js
+++ run.js
@@ -4,7 +4,9 @@ const { existsSync, realpathSync } = require('fs');
 const { homedir } = require('os');
 const { resolve } = require('path');
 
-const ePath = resolve(homedir(), '.electron_build_tools', 'src', 'e');
+const installPath = require('./path');
+
+const ePath = resolve(installPath, 'src', 'e');
 process.argv = process.argv.map((arg) => {
   if (existsSync(arg)) {
     return realpathSync(arg) === realpathSync(__filename) ? ePath : arg;
