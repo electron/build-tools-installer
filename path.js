dbd1b202e0b611b465a9a98fdce1f5e5eb5abf11
Samuel Attard <sattard@anthropic.com>
2025-07-01 17:32:30 -0700

    ok...

--- path.js
+++ path.js
@@ -1,4 +1,5 @@
 const { homedir } = require('os');
 const path = require('path');
 
-module.exports = process.env.ELECTRON_BUILD_TOOLS_ROOT || path.resolve(homedir(), '.electron_build_tools')
+module.exports =
+  process.env.ELECTRON_BUILD_TOOLS_ROOT || path.resolve(homedir(), '.electron_build_tools');
